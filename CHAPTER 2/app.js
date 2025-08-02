a96fe02b20a8028b3c56ead0fbfea96fddbe02c8
MUBEEN UL ISLAM <mubeenulislam207@gmail.com>
2025-08-02 17:22:43 +0500

    update

--- CHAPTER 2/app.js
+++ CHAPTER 2/app.js
@@ -1,4 +1,33 @@
-function submit() {
-  let username = document.getElementById("username").value;
-  document.getElementById("print").innerHTML = username;
-}
+// 1. Declare a variable called username
+var username;
+
+// 2. Declare a variable called myName & assign to it a string
+var myName = "Ali Khan";
+
+// 3. Message variable & display "Hello World"
+var message = "Hello World";
+alert(message);
+
+// 4. Student's bio data
+var studentName = "Ali Khan";
+var studentAge = "20 years old";
+var studentCourse = "Web and Mobile App Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// 5. Special message in alert using one variable
+var specialMessage = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialMessage);
+
+// 6. Email alert
+var email = "example@example.com";
+alert("My email address is " + email);
+
+// 7. Book message
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// 8. Display message in browser
+document.write("Yah! I can write HTML content through JavaScript");

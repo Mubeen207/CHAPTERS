d31f8b6ae559f1c2c45f0b1e877294cd2b360a15
MUBEEN UL ISLAM <mubeenulislam207@gmail.com>
2025-08-02 17:24:55 +0500

    var - let

--- CHAPTER 2/app.js
+++ CHAPTER 2/app.js
@@ -1,32 +1,32 @@
 // 1. Declare a variable called username
-var username;
+let username;
 
 // 2. Declare a variable called myName & assign to it a string
-var myName = "Ali Khan";
+let myName = "Mubeen";
 
 // 3. Message variable & display "Hello World"
 var message = "Hello World";
 alert(message);
 
 // 4. Student's bio data
-var studentName = "Ali Khan";
-var studentAge = "20 years old";
-var studentCourse = "Web and Mobile App Development";
+let studentName = "Mubeen";
+let studentAge = "19 years old";
+let studentCourse = "Web and Mobile App Development";
 
 alert(studentName);
 alert(studentAge);
 alert(studentCourse);
 
 // 5. Special message in alert using one variable
-var specialMessage = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+let specialMessage = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 alert(specialMessage);
 
 // 6. Email alert
-var email = "example@example.com";
+let email = "example@example.com";
 alert("My email address is " + email);
 
 // 7. Book message
-var book = "A smarter way to learn JavaScript";
+let book = "A smarter way to learn JavaScript";
 alert("I am trying to learn from the Book " + book);
 
 // 8. Display message in browser

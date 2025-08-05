600ebc42e308ebf7c83a7ebbbf8e358832af4324
MUBEEN UL ISLAM <mubeenulislam207@gmail.com>
2025-08-05 18:14:20 +0500

    ch 2

--- CHAPTER 5/app.js
+++ CHAPTER 5/app.js
@@ -1,7 +1,33 @@
+// let num1 = 10;
+// let num2 = 15;
+
+// let sum = num1 + num2;
+
+// let result = document.getElementById("result");
+// result.innerHTML = "The sum of " + num1 + " and " + num2 + " is " + sum;
+//-----------------------------------------------------------------------------
+
 let num1 = 10;
-let num2 = 15;
+let num2 = 5;
 
 let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+let mod = num1 % num2;
 
-let result = document.getElementById("result");
-result.innerHTML = "The sum of " + num1 + " and " + num2 + " is " + sum;
+document.getElementById("output").innerHTML =
+  "Sum: " +
+  sum +
+  "<br>" +
+  "Subtraction: " +
+  sub +
+  "<br>" +
+  "Multiplication: " +
+  mul +
+  "<br>" +
+  "Division: " +
+  div +
+  "<br>" +
+  "Modulus: " +
+  mod;
